86a2914e2eda8d7105bf3c44c1a72c942a6fe4dc
Jan Hellar <jhellar@redhat.com>
2017-06-19 10:43:14 +0200

    Mute

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const mute = require('mute');
+const unmute = mute();
+const fhc = require('fh-fhc');
+unmute();
+
 module.exports = {
   init: require('./lib/init'),
-  fhc: require('fh-fhc')
+  fhc
 };
