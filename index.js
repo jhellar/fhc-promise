25c41f43764f526157d65d86f42f02d4c18d7726
Jan Hellar <jhellar@redhat.com>
2017-06-19 10:28:17 +0200

    Fix index.js

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 module.exports = {
-  admin : require('./lib/admin'),
-  auth : require('./lib/auth'),
-  appforms : require('./lib/appforms'),
-  import : require('./lib/import.js'),
-  projects : require('./lib/projects'),
-  services : require('./lib/services'),
-  app : require('./lib/app'),
-  call : require('./lib/call'),
-  connections: require('./lib/connections'),
   init: require('./lib/init'),
   fhc: require('fh-fhc')
 };
